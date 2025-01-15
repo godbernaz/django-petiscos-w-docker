a4269d7eb12e863b3359f0602d22489664d74ae6
godbernaz <bernazbarros2000@gmail.com>
2025-01-15 19:56:05 +0000

    Now im using environment variables for security measures, i forgot to include the secret key in ignore file but will do it tomorrow and change it

--- django_website/settings.py
+++ django_website/settings.py
@@ -1,9 +1,14 @@
 # django_website/settings.py
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -12,13 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%!k)o+xqh*nyhgmj8te0e5radw9gzbhqudfm+enb4^06x!y5-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = env.bool("DJANGO_DEBUG")
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -91,14 +93,7 @@ WSGI_APPLICATION = 'django_website.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+    "default": env.dj_db_url("DATABASE_URL", default="postgres://postgres@db/postgres")
 }
 
 
