c410a45f74991917e85eb0dcb345b248e1cf642a
godbernaz <bernazbarros2000@gmail.com>
2025-01-16 21:33:01 +0000

    I had a problem with testing my Meals models, which I didnt noticed, now everything is working just fine

--- meals/tests.py
+++ meals/tests.py
@@ -1,4 +1,39 @@
 # meals/tests.py
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Meal, Category
+
+class MealTests(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(
+            category_name='Filetes'
+        )
+        cls.meal = Meal.objects.create(
+            meal_name='Francesinha',
+            category=cls.category,
+            price='30.00',
+            description='ola', 
+        )
+        
+    def test_meal_listing(self):
+        self.assertEqual(f"{self.meal.meal_name}", 'Francesinha')
+        self.assertEqual(f"{self.meal.category.category_name}", 'Filetes')
+        self.assertEqual(f"{self.meal.price}", '30.00')
+        self.assertEqual(f"{self.meal.description}", 'ola')
+        
+    def test_meal_list_view(self):
+        response = self.client.get(reverse('meal_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Francesinha')
+        self.assertTemplateUsed(response, 'meals/meal_list.html')
+        
+    def test_meal_detail_view(self):
+        response = self.client.get(self.meal.get_absolute_url())
+        no_response = self.client.get('/meals/12345')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Francesinha')
+        self.assertTemplateUsed(response, 'meals/meal_detail.html')
