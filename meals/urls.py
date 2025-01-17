18fc40adc1cb1d19dcd3d66af7c2d5a5cb3b2286
godbernaz <bernazbarros2000@gmail.com>
2025-01-17 14:56:30 +0000

    Now search bar is working, which was easy, users can search via the meals name, or for the meals category

--- meals/urls.py
+++ meals/urls.py
@@ -1,8 +1,9 @@
 # meals/urls.py
 from django.urls import path
-from .views import MealListView, MealDetailView
+from .views import MealListView, MealDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', MealListView.as_view(), name="meal_list"),
     path('<uuid:pk>/', MealDetailView.as_view(), name="meal_detail"),
+    path('search/', SearchResultsListView.as_view(), name="search_results"),
 ]
--- meals/views.py
+++ meals/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Meal, Category
@@ -22,3 +23,13 @@ class MealDetailView(
     login_url = 'account_login'
     permission_required = 'meals.special_status'
 
+class SearchResultsListView(ListView):
+    model = Meal
+    context_object_name = 'meal_list'
+    template_name = 'meals/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Meal.objects.filter(
+            Q(meal_name__icontains=query) | Q(category__category_name__icontains=query)
+        )
