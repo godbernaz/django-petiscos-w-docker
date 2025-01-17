454d601ad3551f3f262225aa76ac7e7b4d8e3182
godbernaz <bernazbarros2000@gmail.com>
2025-01-17 11:18:09 +0000

    Reviews models finished, made sure to make tests for it, and everything working as it should

--- meals/tests.py
+++ meals/tests.py
@@ -1,21 +1,36 @@
 # meals/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Meal, Category
+
+from .models import Meal, Category,  Review
 
 class MealTests(TestCase):
     
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123',
+        )
+        
         cls.category = Category.objects.create(
             category_name='Filetes'
         )
+        
         cls.meal = Meal.objects.create(
             meal_name='Francesinha',
             category=cls.category,
             price='30.00',
             description='ola', 
+        )
+        
+        cls.review = Review.objects.create(
+            meal=cls.meal,
+            user_review = cls.user,
+            review='Fantastico Lininha',
         )
         
     def test_meal_listing(self):
@@ -36,4 +51,5 @@ class MealTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Francesinha')
+        self.assertContains(response, 'Fantastico Lininha')
         self.assertTemplateUsed(response, 'meals/meal_detail.html')
