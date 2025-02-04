4d4067d39f564fd1f7dcdc644ffb48485eabeef3
Bernaz Barros <bernazbarros2000@gmail.com>
2025-02-04 17:28:33 +0000

    Now orders are being made and admin can see it on admin panel, but there is a little bit of work needed

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,9 @@
 # orders/urls.py
 from django.urls import path
-from .views import OrderDetailsView
+from .views import OrderDetailsView, ConfirmOrderView, OrderSuccessView
 
 urlpatterns = [
     path('details/', OrderDetailsView.as_view(), name="order_detail"),
+    path('orders/confirm/', ConfirmOrderView.as_view(), name='confirm_order'),
+    path('success/<uuid:order_id>/', OrderSuccessView.as_view(), name="order_success"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from carts.models import Cart
 from accounts.models import UserBilling
 from accounts.forms import UserBillingForm
+from orders.models import Order, OrderItem
 
 class OrderDetailsView(LoginRequiredMixin, TemplateView):
     template_name = 'orders/order_detail.html'
@@ -20,6 +21,11 @@ class OrderDetailsView(LoginRequiredMixin, TemplateView):
         return context
 
     def post(self, request, *args, **kwargs):
+        if 'billing_profile' in request.POST:
+            billing_profile_id = request.POST.get('billing_profile')
+            billing_profile = get_object_or_404(UserBilling, id=billing_profile_id, user=request.user)
+            return redirect('confirm_order', billing_profile_id=billing_profile.id)
+        
         form = UserBillingForm(request.POST)
         if form.is_valid():
             billing_profile = form.save(commit=False)
@@ -31,3 +37,36 @@ class OrderDetailsView(LoginRequiredMixin, TemplateView):
         context['billing_form'] = form
         return self.render_to_response(context)
 
+class ConfirmOrderView(LoginRequiredMixin, View):
+    def post(self, request):
+        billing_profile_id = request.POST.get('billing_profile')
+        cart = get_object_or_404(Cart, user=request.user)
+        billing_profile = get_object_or_404(UserBilling, id=billing_profile_id, user=request.user)
+
+        if not cart.items.exists():
+            return redirect('order_detail')
+
+        order = Order.objects.create(
+            user=request.user,
+            billing_info=billing_profile,
+            total_price=cart.total_cart_price
+        )
+
+        for item in cart.items.all():
+            OrderItem.objects.create(
+                order=order,
+                meal=item.meal,
+                quantity=item.quantity,
+                price=item.meal.price
+            )
+
+        cart.items.all().delete()
+        return redirect('order_success', order_id=order.id)
+
+class OrderSuccessView(LoginRequiredMixin, TemplateView):
+    template_name = 'orders/order_success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['order'] = get_object_or_404(Order, id=self.kwargs['order_id'], user=self.request.user)
+        return context
