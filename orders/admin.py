7c4ea919635e847fbd425e49ca437c5c28dd5c97
Bernaz Barros <bernazbarros2000@gmail.com>
2025-02-04 15:01:05 +0000

    Users now can create an userbilling profile when checking out the order, and can select different userbilling profiles already created

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1  # Permite adicionar itens diretamente no pedido
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'billing_info', 'created_at', 'total_price')
+    list_filter = ('created_at', 'user')
+    search_fields = ('user__username', 'id')
+    inlines = [OrderItemInline]  # Permite visualizar itens do pedido na mesma página
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'meal', 'quantity', 'price', 'total_price')
+    search_fields = ('order__id', 'meal__meal_name')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from carts.models import Cart
 from accounts.models import UserBilling
+from accounts.forms import UserBillingForm
 
 class OrderDetailsView(LoginRequiredMixin, TemplateView):
     template_name = 'orders/order_detail.html'
@@ -11,7 +12,22 @@ class OrderDetailsView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         cart = get_object_or_404(Cart, user=self.request.user)
         billing_profiles = UserBilling.objects.filter(user=self.request.user)
+        form = UserBillingForm()
         
         context['cart'] = cart
         context['billing_profiles'] = billing_profiles
+        context['billing_form'] = form
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = UserBillingForm(request.POST)
+        if form.is_valid():
+            billing_profile = form.save(commit=False)
+            billing_profile.user = request.user
+            billing_profile.save()
+            return redirect('order_detail')
+        
+        context = self.get_context_data()
+        context['billing_form'] = form
+        return self.render_to_response(context)
+
